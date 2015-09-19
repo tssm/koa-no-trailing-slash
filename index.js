fe305e27bb730a952720c476d16a29a3c9fd2aaf
Tae Sandoval Murgan <tae@mylodon.cl>
2015-09-19 20:05:27 -0300

    First approach

---
+++ index.js
@@ -0,0 +1,11 @@
+module.exports = function noTrailingSlash() {
+	return function *(next) {
+		var url = this.request.url
+		yield next
+		if (/\/$/.test(url)) {
+			var trailingSlashPosition = url.length - 1
+			this.status = 301
+			this.response.redirect(url.slice(0, trailingSlashPosition))
+		}
+	}
+}()
