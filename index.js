e00fe7a91d74b0720b1d8fb5ed86ef7fbbb49ff3
Tae Sandoval Murgan <tae@mylodon.cl>
2015-09-21 02:14:12 -0300

    Put the yield statement after the redirect

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 module.exports = function noTrailingSlash() {
 	return function *(next) {
 		var url = this.request.url
-		yield next
 		if (url != '/' && /\/$/.test(url)) {
 			var trailingSlashPosition = url.length - 1
 			this.response.status = 301
 			this.response.redirect(url.slice(0, trailingSlashPosition))
+			return
 		}
+		yield next
 	}
 }()
